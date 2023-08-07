8829f17d9a05136106ac4fbb68aacf705aa440f6
leechangjoon <2018110021@dongguk.edu>
2023-08-07 22:39:14 +0900

    Design: Home page setting + 폴더 구조 수정

---
+++ blink_FE/src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import React, { useRef, useEffect } from "react";
+import styled from "styled-components";
+
+const HomeContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+export default function Home() {
+  return <HomeContainer>안녕</HomeContainer>;
+}
