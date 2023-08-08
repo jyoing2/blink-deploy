10e8d74f3fde743be25bc5c896aa34426bbebdf0
ohchanju <ohchanju3@naver.com>
2023-08-08 20:52:39 +0900

    feat: login 컴포넌트, 페이지 폴더 생성

---
+++ blink_FE/src/styles/theme.jsx
@@ -0,0 +1,12 @@
+import { css } from "styled-components";
+
+const color = {
+  kakao: "#FFE812",
+  naver: "#06C755",
+  blue1: "#3865BF",
+  blue2: "#34446D",
+  navblue: "#1B2130",
+  navlightblue: "#0F1E33",
+};
+
+export const theme = { color };
