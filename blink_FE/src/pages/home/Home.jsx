52a3d3e6012f3e4650615b7b9a375140fe9b2dc4
leechangjoon <2018110021@dongguk.edu>
2023-08-11 17:33:08 +0900

    fix: google map api fixing

--- blink_FE/src/pages/home/Home.jsx
+++ blink_FE/src/pages/home/Home.jsx
@@ -1,10 +1,11 @@
 import React, { useRef, useEffect } from "react";
 import styled from "styled-components";
 import KMap from "../../components/post/KMap";
+import Map from "../../components/post/Map";
 import PostList from "../../components/post/PostList";
 
 const HomeContainer = styled.div`
-  /* margin-left: 200px; */
+  margin-left: 220px;
   width: 1920px;
   height: 1080px;
   background-color: green;
@@ -16,7 +17,7 @@ export default function Home() {
   return (
     <HomeContainer>
       <PostList />
-      <KMap />
+      <Map />
     </HomeContainer>
   );
 }
