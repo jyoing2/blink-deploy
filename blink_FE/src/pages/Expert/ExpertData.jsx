3d99d007b1ebb7a6125125b7f06678a94542c5cf
hye914 <kimhyesu0914@gmail.com>
2023-08-12 22:24:24 +0900

    modify : data,style파일 분리

---
+++ blink_FE/src/pages/Expert/ExpertData.jsx
@@ -0,0 +1,58 @@
+export const CardData = [
+  {
+    imageUrl: "img/lawyer5.jpg",
+    job: "변호사",
+    userId: "김지현",
+    experience:
+      "2023~ 멋쟁이 사자처럼 대학 입학 2023~ 피자스쿨 대학원 입학 2024~ 피자스쿨 마스터 예정",
+  },
+  {
+    imageUrl: "img/lawyer7.jpg",
+    job: "변호사",
+    userId: "박민준",
+    experience:
+      "2023~ 멋쟁이 사자처럼 대학 입학 2023~ 피자스쿨 대학원 입학 2024~ 피자스쿨 마스터 예정",
+  },
+  {
+    imageUrl: "img/lawyer8.jpg",
+    job: "변호사",
+    userId: "이서연",
+    experience:
+      "2023~ 멋쟁이 사자처럼 대학 입학 2023~ 피자스쿨 대학원 입학 2024~ 피자스쿨 마스터 예정",
+  },
+  {
+    imageUrl: "img/lawyer6.jpg",
+    job: "변호사",
+    userId: "최준호",
+    experience:
+      "2023~ 멋쟁이 사자처럼 대학 입학 2023~ 피자스쿨 대학원 입학 2024~ 피자스쿨 마스터 예정",
+  },
+  {
+    imageUrl: "img/lawyer9.jpg",
+    job: "변호사",
+    userId: "정유진",
+    experience:
+      "2023~ 멋쟁이 사자처럼 대학 입학 2023~ 피자스쿨 대학원 입학 2024~ 피자스쿨 마스터 예정",
+  },
+  {
+    imageUrl: "img/lawyer1.jpg",
+    job: "변호사",
+    userId: "강도현",
+    experience:
+      "2023~ 멋쟁이 사자처럼 대학 입학 2023~ 피자스쿨 대학원 입학 2024~ 피자스쿨 마스터 예정",
+  },
+  {
+    imageUrl: "img/lawyer2.png",
+    job: "변호사",
+    userId: "임아름",
+    experience:
+      "2023~ 멋쟁이 사자처럼 대학 입학 2023~ 피자스쿨 대학원 입학 2024~ 피자스쿨 마스터 예정",
+  },
+  {
+    imageUrl: "img/lawyer3.jpg",
+    job: "변호사",
+    userId: "한성민",
+    experience:
+      "2023~ 멋쟁이 사자처럼 대학 입학 2023~ 피자스쿨 대학원 입학 2024~ 피자스쿨 마스터 예정",
+  },
+];
--- blink_FE/src/pages/Expert/style.jsx
+++ blink_FE/src/pages/Expert/style.jsx
@@ -1,35 +1,36 @@
-const Container = styled.div`
+import styled from "styled-components";
+
+export const Container = styled.div`
   width: 89%;
   height: 884px;
   margin-left: 200px;
 `;
 
-const Explain = styled.p`
+export const Explain = styled.p`
   padding: 40px;
   color: black;
   font-size: 30px;
   font-weight: 600;
 `;
 
-const Range = styled.select`
+export const Range = styled.select`
   width: 100px;
   height: 30px;
   margin-left: 40px;
   font-size: 15px;
 `;
 
-const Outline = styled.div`
+export const Outline = styled.div`
   width: 96%;
-  background-color: pink;
   margin: 40px;
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
 `;
 
-const Card = styled.div`
+export const Card = styled.div`
   width: 400px;
-  height: 400px;
+  height: 600px;
   border: 1px solid black;
   display: flex;
   flex-direction: column;
@@ -39,12 +40,35 @@ const Card = styled.div`
   background-color: #d9d9d9;
 `;
 
-const CardImage = styled.img`
-  width: 80%;
-  height: auto;
+export const CardImage = styled.img`
+  width: 350px;
+  height: 350px;
+  margin-top: 15px;
+  border-radius: 15px;
 `;
 
-const CardText = styled.p`
+export const CardJob = styled.p`
+  font-size: 30px;
+  margin-top: 20px;
+  font-weight: 700;
+  position: relative;
+  right: 80px;
+`;
+
+export const CardEx = styled.p`
   font-size: 16px;
   margin-top: 10px;
+  font-weight: 600;
+`;
+
+export const Wrap = styled.div`
+  display: flex;
+`;
+
+export const Wrapping = styled.div`
+  display: flex;
+  position: relative;
+  right: 45px;
+  margin-top: 15px;
+  font-size: 20px;
 `;
