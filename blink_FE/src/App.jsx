c4d09e17210d9b4068a1682336dc68ee418867fa
hye914 <kimhyesu0914@gmail.com>
2023-08-12 21:49:06 +0900

    충돌 방지용 커밋

--- blink_FE/src/App.jsx
+++ blink_FE/src/App.jsx
@@ -8,6 +8,7 @@ import { Layout } from "./components/Layout/Layout";
 import Post from "./components/post/Post";
 import Signin from "./pages/Login/signin";
 import KMap from "./components/post/KMap";
+import ExpertList from "./pages/Expert/ExpertList";
 
 // Create a separate component for GlobalStyle to fix the hook error
 const GlobalStyleComponent = createGlobalStyle`
@@ -33,10 +34,10 @@ function App() {
     <>
       <GlobalStyleComponent />
       <Routes>
-        {/* <Route path="/" element={<Layout />}> */}
-        {/* <Route index element={<Home />} /> */}
+        <Route path="/" element={<Layout />} />
+        <Route index element={<ExpertList />} />
         {/* <Route index element={<KMap />} /> */}
-        <Route index element={<Post />} />
+        {/* <Route index element={<Post />} /> */}
         {/* <Route index element={<Signin />}></Route> */}
         {/* </Route> */}
       </Routes>
