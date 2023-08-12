b960042e73382a2e8d173457c0d4b1c315d9f15d
hye914 <kimhyesu0914@gmail.com>
2023-08-12 20:46:38 +0900

    feat: 변호사 카드 완성

---
+++ blink_FE/src/pages/Expert/ExpertDetail.jsx
@@ -0,0 +1,44 @@
+const cardData = [
+  {
+    imageUrl: "./img/lawyer.jpg",
+    userId: "김혜수",
+    experience: "5년",
+  },
+  {
+    imageUrl: "",
+    userId: "변호사 김23",
+    experience: "3년",
+  },
+  {
+    imageUrl: "",
+    userId: "변호사 김23",
+    experience: "3년",
+  },
+  {
+    imageUrl: "",
+    userId: "변호사 김23",
+    experience: "3년",
+  },
+  {
+    imageUrl: "",
+    userId: "변호사 김23",
+    experience: "3년",
+  },
+  {
+    imageUrl: "",
+    userId: "변호사 김23",
+    experience: "3년",
+  },
+  {
+    imageUrl: "",
+    userId: "변호사 김23",
+    experience: "3년",
+  },
+  {
+    imageUrl: "",
+    userId: "변호사 김23",
+    experience: "3년",
+  },
+];
+
+export { cardData };
