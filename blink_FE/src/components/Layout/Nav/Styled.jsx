11fde33df359bfb3ab27feb40b64f2836f81cd74
오찬주 <ohchanju3@naver.com>
2023-08-15 03:24:37 +0900

    design: navbar 구현, 경로 설정 아직임

--- blink_FE/src/components/Layout/Nav/Styled.jsx
+++ blink_FE/src/components/Layout/Nav/Styled.jsx
@@ -1,4 +1,5 @@
 //nav style
+import { css } from "styled-components";
 import styled from "styled-components";
 
 export const NavContainer = styled.div`
@@ -14,37 +15,95 @@ export const NavContainer = styled.div`
   align-items: center;
 
   .search {
-    width: 43px;
-    height: 44.84px;
-    color: white;
-  }
-
-  .line {
-    color: white;
-    size: 88px;
+    font-size: 43px;
+    color: #d8d8d8;
+    cursor: pointer;
   }
 
   .chat {
     width: 31px;
     height: 31px;
-    color: white;
+    color: #d8d8d8;
     margin-top: 3px;
+    cursor: pointer;
   }
-`;
 
-export const NavItem = styled.div`
-  padding: 10px;
-  cursor: pointer;
-  &:hover {
-    background-color: lightblue;
+  .bell {
+    width: 35px;
+    height: 35px;
+    color: #d8d8d8;
+    margin-top: 3px;
+    cursor: pointer;
+  }
+
+  .Person {
+    width: 35px;
+    height: 35px;
+    color: #d8d8d8;
+    cursor: pointer;
   }
 `;
 
 export const NavLogo = styled.div`
   display: flex;
   justify-content: center;
+  cursor: pointer;
 `;
 
 export const NavLogoImage = styled.img`
   width: 150px;
+  &:hover {
+    opacity: 0.7;
+  }
+`;
+
+export const NavIcon = styled.div`
+  width: 100%;
+  height: 70px;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+  color: white;
+  cursor: pointer;
+  transition: box-shadow 0.3s ease;
+  &:hover {
+    /* background-color: #34446d;
+    box-shadow: 7px 3px 3px white; */
+    opacity: 0.3;
+
+    svg {
+      color: white;
+    }
+  }
+
+  p {
+    font-size: x-small;
+    margin-top: 10px;
+  }
+`;
+
+//user 프로필
+
+export const UserProfileImage = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  object-fit: cover;
+`;
+
+export const Profile = styled.div`
+  width: 50px;
+  height: 50px;
+  color: white;
+  display: flex;
+  position: relative;
+  /* border: 1px solid white; */
+  border-radius: 30px;
+  margin-top: 170px;
+  justify-content: center;
+  cursor: pointer;
+  &:hover {
+    opacity: 0.7;
+  }
 `;
