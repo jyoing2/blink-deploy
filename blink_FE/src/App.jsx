a50da7b07d4a206b4dcaabd3f56be21f1b5f438c
오찬주 <ohchanju3@naver.com>
2023-08-15 14:51:11 +0900

    feat: intro 페이지 생성 후 작업 중

--- blink_FE/src/App.jsx
+++ blink_FE/src/App.jsx
@@ -15,6 +15,7 @@ import Nav from "./components/Layout/Nav/Nav";
 import Signup from "./pages/Auth/Signup/signup";
 import { Outlet } from "react-router-dom";
 import ExpertSignup from "./pages/Auth/Signup/ExpertSignup";
+import Intro from "./pages/Intro/intro";
 
 /* 추가적인 리셋이나 스타일 조정을 여기에 추가할 수 있습니다. */
 const GlobalStyleComponent = createGlobalStyle`
@@ -51,8 +52,9 @@ function App() {
         {/* <Route index element={<KMap />} /> */}
         <Route index element={<Signin />}>
           {/* <Route index element={<Post />} /> */}
-          {/* <Route index element={<Signup />}></Route> */}
-          {/* <Route index element={<ExpertSignup />}></Route> */}
+          {/* <Route index element={<Intro />}> */}
+          {/* <Route index element={<Signup />}> */}
+          {/* <Route index element={<ExpertSignup />}> */}
           {/* #4 변호사 페이지 렌더링 코드 */}
           {/* <Route path="/expert" element={<ExpertList />}> */}
           {/* <Route
